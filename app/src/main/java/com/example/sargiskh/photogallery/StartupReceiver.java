598d5a353bfe70e5ed431e533bb799d3408ef881
Sargis Khlopuzyan <sargiskh@energizeglobal.com>
2016-12-05 18:28:48 +0400

    chapter 27

---
+++ app/src/main/java/com/example/sargiskh/photogallery/StartupReceiver.java
@@ -0,0 +1,19 @@
+package com.example.sargiskh.photogallery;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class StartupReceiver extends BroadcastReceiver {
+
+    private static final String TAG = "StartupReceiver";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.e(TAG, "Received broadcast intent: " + intent.getAction());
+
+        boolean isOn = QueryPreferences.isAlarmOn(context);
+        PollService.setServiceAlarm(context, isOn);
+    }
+}
